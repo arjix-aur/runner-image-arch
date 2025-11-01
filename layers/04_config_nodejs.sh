c338ee30d10f4b8e2bfc991ade30a5922a48bb16
ArjixWasTaken <me@arjix.dev>
2025-11-01 20:05:39 +0200

    remove pnpm steps

--- layers/04_config_nodejs.sh
+++ layers/04_config_nodejs.sh
@@ -5,12 +5,3 @@ USER="$1"
 for home in "/root" "$HOME"; do
     echo ". /usr/share/nvm/init-nvm.sh" | sudo tee -a "$home/.bashrc"
 done
-
-. /usr/share/nvm/init-nvm.sh
-sudo corepack enable
-
-for user in "root" "$USER"; do
-    sudo runuser -u "$user" -- bash -i -c 'pnpm setup'
-    sudo runuser -u "$user" -- bash -i -c 'pnpm config set --global dangerouslyAllowAllBuilds true'
-    sudo runuser -u "$user" -- bash -i -c 'pnpm install -g grunt gulp parcel-bundler typescript newman vercel webpack webpack-cli lerna netlify-cli'
-done
